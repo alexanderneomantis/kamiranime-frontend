54c85783141e0987bc1bb705697671ed4172b9f5
alex <alexcarvajal2404@gmail.com>
2022-05-31 16:55:40 -0500

    agregadas mas componentes y filtros actualizados

---
+++ src/components/BreadCrumb.js
@@ -0,0 +1,16 @@
+import {Box, Typography} from "@mui/material";
+import {useLocation} from "react-router-dom";
+
+export default function BreadCrumb() {
+  const {pathname} = useLocation()
+
+  function formatPath(txt) {
+    return txt.replace(/\b\w/g, c => c.toUpperCase()).replace('/', '')
+  }
+  console.log(useLocation())
+  return (
+    <Box sx={{ backgroundColor: theme => theme.palette.secondary.main, p: 5, display: 'flex', justifyContent: 'center' }}>
+      <Typography variant='h4'>Home / {formatPath(pathname)}</Typography>
+    </Box>
+  )
+}
--- src/pages/acrilicos/index.js
+++ src/pages/acrilicos/index.js
@@ -1,8 +1,13 @@
 import {styled} from "@mui/material/styles";
 import Page from '../../components/Page'
 import Product from "../../components/Product";
-import { Container, Grid} from "@mui/material";
-
+import {Box, Container, Grid, Typography} from "@mui/material";
+import SortDesktop from "../../components/products/SortDektop";
+import SortMobile from "../../components/products/SortMobile";
+import FiltersMobile from "../../components/products/FiltersMobile";
+import FiltersDesktop from "../../components/products/FiltersDesktop";
+import {useState} from "react";
+import BreadCrumb from "../../components/BreadCrumb";
 
 const APP_BAR_MOBILE = 64;
 const APP_BAR_DESKTOP = 88;
@@ -17,19 +22,34 @@ const RootStyle = styled(Page)(({theme}) => ({
 }));
 
 
-export default function Acrilicos() {
+export default function Figuras() {
+  const [drawer, setDrawer] = useState(false);
 
   return (
-    <RootStyle title='Acrilicos | Kamiranime'>
+    <RootStyle title='Figuras | Kamiranime'>
+      <BreadCrumb />
       <Container>
         <Grid container>
-          {
-            [...Array(10)].map((el, i) => (
-              <Grid item xs={12} sm={6} md={3} key={i + 12} sx={{display: 'flex', justifyContent: 'center'}}>
-                <Product/>
-              </Grid>
-            ))
-          }
+          <Grid item xs={12} md={3}>
+            <FiltersDesktop />
+            <FiltersMobile setDrawer={setDrawer} drawer={drawer} />
+          </Grid>
+          <Grid item xs={12} md={9}>
+            <SortDesktop />
+            <SortMobile setDrawer={setDrawer} />
+
+            <Box sx={{ p: 3 }}>
+              <Grid container spacing={3}>
+                {
+                  [...Array(10)].map((el, i) => (
+                    <Grid item xs={12} sm={6} md={3} key={i + 12} sx={{display: 'flex', justifyContent: 'center'}}>
+                      <Product/>
+                    </Grid>
+                  ))
+                }</Grid>
+            </Box>
+          </Grid>
+
         </Grid>
       </Container>
     </RootStyle>
